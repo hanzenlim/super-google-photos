48134cfed846c2c37e53cf47c2010b9a9949d543
Hanzen Lim <hlim@zipdrug.com>
2020-05-27 22:02:56 -0400

    Add save picture endpoint

--- handler.js
+++ handler.js
@@ -1,12 +1,24 @@
 "use strict";
 
 var textract = require("aws-sdk").textract;
+const savePictures = require("./savePictures");
 
 module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: `bye Hello, ${event} the current time is ${new Date().toTimeString()}.`,
+    }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.savePictures = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `save pictures`,
     }),
   };
 
