6b509ade961046369511e62f23be110997eb50fd
Hanzen Lim <hlim@zipdrug.com>
2020-05-27 21:47:56 -0400

    Fix issue

--- handler.js
+++ handler.js
@@ -1,10 +1,12 @@
 "use strict";
 
+var textract = require("aws-sdk").textract;
+
 module.exports.endpoint = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+      message: `bye Hello, ${event} the current time is ${new Date().toTimeString()}.`,
     }),
   };
 
